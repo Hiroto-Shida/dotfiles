678e4836ffabe6ee99d1b3d3f718b5f634b02029
Hiroto-Shida <uecshida6295@gmail.com>
2026-05-01 09:00:32 +0900

    nvim hide_dotfiles=false

---
+++ private_dot_config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,9 @@
+---@type LazySpec
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = function(_, opts)
+    opts.filesystem = opts.filesystem or {}
+    opts.filesystem.filtered_items = opts.filesystem.filtered_items or {}
+    opts.filesystem.filtered_items.hide_dotfiles = false
+  end,
+}
